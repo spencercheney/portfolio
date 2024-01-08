0c099567729b5e492ae99a6f75bb0aa0fd50a095
Spencer Cheney <spencercheney@gmail.com>
2024-01-08 00:58:46 -0700

    made sidebar responsive

--- components/header/header.js
+++ components/header/header.js
@@ -6,15 +6,14 @@ export default function Header({ location, isInitialized }) {
   const toastDispatch = useToast()
   //what will be displayed in the toast
   const toastContent = <span>
-    I've always been entertained by the idea of magic. Believing in powers that can't be explained. This includes
-    genies.
+    I've always been entertained by the idea of magic, believing in powers that can't be explained. I've always thought 
+    the most interesting magically being is the genie.
     <br /><br />
     One of the struggles I often face is my last name getting mispronouced. It's been pronouced ChAYnAY, ChAYnEE, and even
     ShanAYnAY which are all incorrect. The correct pronouciation is ChEEnEE. The easiest way to remember it is that it's
     pronouced the same way as genie but with a ch instead of g, Ch-enie. 
     <br /><br />
-    The fact that I enjoy magic and that genies help pronouce my last name is perfect. That's why I've add the lamp and 
-    gave this website the domain name.
+    While genies may not actually exist, they are magically able to help people remember my name. 
   </span>
 
   const module = useModule(isInitialized)
--- components/sidebar.js
+++ components/sidebar.js
@@ -1,16 +1,24 @@
+"use client"
+
 import styles from "../styles/sidebar.module.css"
 import BasicComponent from './basic'
-import Link from 'next/link'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faUser, faCode, faMagnifyingGlassChart, faFileArrowDown, faGamepad } from "@fortawesome/free-solid-svg-icons"
+import { faUser, faCode, faFileArrowDown, faHouse, faBars} from "@fortawesome/free-solid-svg-icons"
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons"
+import { useEffect, useRef, useState } from "react"
 
 export default function Sidebar({ goToLink }) {
+  const list = useRef(null)
+
   return (
     <div className={styles.sidebar}>
       <BasicComponent>
-        <div className={styles.list}>
+        <div className={styles.list} ref={list}>
+          <button className={styles.link} onClick={ () => goToLink("/", true) }>
+            <FontAwesomeIcon icon={faHouse} />
+            <span>Home</span>
+          </button>
           <button className={styles.link} onClick={ () => goToLink("/aboutMe", false) }>
             <FontAwesomeIcon icon={faUser} />
             <span>About Me</span>
@@ -28,7 +36,42 @@ export default function Sidebar({ goToLink }) {
             <span>Contact Me</span>
           </button>
         </div>
+
+        <DisplayControl list={list}/>
       </BasicComponent>
+    </div>
+  )
+}
+
+function DisplayControl({list}) {
+  const [show, setShow] = useState(false)
+  const displayControl = useRef(null)
+
+  useEffect(() => {
+    const isControlOpen = getComputedStyle(displayControl.current).display != "none"
+
+    if(!isControlOpen || show) {
+      list.current.style.display = "inline-block"
+    } else {
+      list.current.style.display = "none"
+    }
+  }, [show])
+
+  return (
+    <div className={styles.displayControl} ref={displayControl}>
+      {!show && 
+        <button className={styles.link} onClick={() => setShow(true)}>
+          <FontAwesomeIcon icon={faBars} />
+          Show Navigation
+        </button>
+      }
+      
+      {show &&
+        <button className={styles.link} onClick={() => setShow(false)}>
+          <FontAwesomeIcon icon={faBars} />
+          Hide Navigation
+        </button>
+      }
     </div>
   )
 }
